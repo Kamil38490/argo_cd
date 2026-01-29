ff23ba52d66debec53cdc4d30b6c9d837f932529
kamil38490 <kamil38490@gmail.com>
2026-01-29 10:38:29 +0100

    zmieniamy numer obrazy na 122

--- docker/app.py
+++ docker/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from ggg to jest dobry dzień "
+    return "Hello from ggg to jest dobry dzień nie zle "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
